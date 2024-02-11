d5a3053583a190d37c4ac1ddf0d6b02567c74181
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-11 11:33:04 -0800

    feat: Add ability to view contestants index page and see all contestants and the projects they've worked on

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -21,12 +21,16 @@ RSpec.describe 'Contestants Index Page' do
     kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
     news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
     upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
     ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: news_chic.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
     ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
     ContestantProject.create(contestant_id: erin.id, project_id: lit_fit.id)
@@ -41,6 +45,10 @@ RSpec.describe 'Contestants Index Page' do
     expect(page).to have_content(upholstery_tux.name)
     expect(boardfit.name).to appear_before(lit_fit.name)
     expect(page).to have_content(lit_fit.name)
+    within("##{erin.id}") do
+      expect(page).to have_content(news_chic.name)
+      expect(page).to have_content(lit_fit.name)
+    end
   end
 end
 
