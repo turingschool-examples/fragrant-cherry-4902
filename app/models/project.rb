45fb5bb165db850d7dd4c9ab2a3b6da5d754cebd
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-11 10:46:06 -0800

    feat: Add #project_challenge_theme method to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def project_challenge_theme
+    Challenge.find(self.challenge_id).theme
+  end
 end
