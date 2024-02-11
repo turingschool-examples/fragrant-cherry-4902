57a4e7cf428ca7f49a1a41c750726911348d2d39
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-11 00:00:19 -0700

    Feature: In a projects show page: displays number of contestants on this project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def num_of_contestants
+    contestants.count
+  end
 end
