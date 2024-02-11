75994a0f4742db48989287086a91e28d69d43ae5
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-11 10:47:06 -0800

    feat: Add ability to visit project show page and see the project name, material, and the them for the associated challenge

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Projects Show Page' do
+  it 'can visit a projects show page and see the projects name and material' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+  end
+
+  it 'also shows the theme of the challenge that the project is associated with' do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(recycled_material_challenge.theme)
+  end
+end
