9001956c4386ef070f83d08331b57908481d0a0e
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-11 11:14:28 -0800

    feat: Add model method #find_contestant_projects to Contestant model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,8 @@
 class Contestant < ApplicationRecord
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def find_contestant_projects
+    Project.joins(:contestants).where("contestants.id = #{self.id}")
+  end
 end
