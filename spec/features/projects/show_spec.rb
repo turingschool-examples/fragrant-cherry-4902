1da0f41f6e631f3d804df0e108edde64636bba80
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-11 11:38:15 -0800

    test: Add tests for number of contestants on a project for projects show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -21,4 +21,29 @@ RSpec.describe 'Projects Show Page' do
 
     expect(page).to have_content(recycled_material_challenge.theme)
   end
+
+  it 'can visit a projects show page and see the number of contestants on the project' do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    
+
+    visit "/projects/#{news_chic}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+    
+    visit "/projects/#{upholstery_tux}"
+
+    expect(page).to have_content("Number of Contestants: 1")
+  end
 end
