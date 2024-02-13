a890397df50d945e3657a906e6eb1dae20fb54c2
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-13 00:22:57 -0600

    Fix: relationship tests

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,3 +1,4 @@
 class Contestant < ApplicationRecord
-
+  has_many :contestant_projects, dependent: :destroy
+  has_many :projects, through: :contestant_projects
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :challenge
+  has_many :contestant_projects, dependent: :destroy
+  has_many :contestants, through: :contestant_projects
 end
