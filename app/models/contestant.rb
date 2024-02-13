cb8a7c50b2b36ce32408ff7155049b48dabc6eb6
Logan Finnegan <logantfinnegan@protonmail.com>
2024-02-13 17:42:24 -0600

    extension

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,12 @@
 class Contestant < ApplicationRecord
   has_many :contestant_projects, dependent: :destroy
   has_many :projects, through: :contestant_projects
+
+  def self.contestant_count
+    self.count
+  end
+
+  def self.average_exp
+    self.average(:years_of_experience)
+  end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -40,5 +40,66 @@ RSpec.describe 'project show', type: :feature do
       #   Challenge Theme: Apartment Furnishings)
       expect(page).to have_content(lit_fit.challenge.theme)
     end
+
+    # User Story 3 of 3
+    it "displays number of contestants" do 
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+
+      # When I visit a project's show page
+      visit "/projects/#{lit_fit.id}"
+      # I see a count of the number of contestants on this project
+      expect(page).to have_content("Contestants on this project: 0")
+    end
+
+    # User Story Extension 1 - Average years of experience for contestants by project
+    it "can display average exp" do 
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+      # When I visit a project's show page
+      visit "/projects/#{upholstery_tux.id}"
+      # I see the average years of experience for the contestants that worked on that project
+      expect(page).to have_content("Average Contestant Exp: 10 Years")
+    end
   end
 end
