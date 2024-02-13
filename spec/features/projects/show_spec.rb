5f771831efded57f51d19be9ce00eeb7ec2429f1
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-13 13:34:59 -0700

    US 1 complete

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -14,12 +14,4 @@ RSpec.describe 'Project Show Page' do
         expect(page).to have_content("Recycled Material") # challenge theme
     end
 
-
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
 end
