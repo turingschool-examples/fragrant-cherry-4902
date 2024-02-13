e656991b4cc41e96a6ec07dfebeb3ff952de961f
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-13 01:06:02 -0600

    feat: view pages, test: challenge theme

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects, dependent: :destroy
   has_many :contestants, through: :contestant_projects
+
+  def challenge_theme
+    challenge.theme
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "instance methods" do
+    let!(:challenge_1) {Challenge.create!(theme: "Apartment Furnishings", project_budget: 550)}
+    let!(:project_1) {challenge_1.create!(name: "Litfit", material: "Lamp Shade")}
+
+    it "list challenge's theme" do
+      expect(project_1.challenge_theme).to eq("Apartment Furnishings")
+    end
+  end
 end
