d41a046015da04082f68d9d60f6aede276769791
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-13 14:32:04 -0700

    fixed model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,6 @@ class ProjectsController < ApplicationController
     
     def show
         @project = Project.find(params[:id])
-        @project_count = Project.count
+        @project_count = Project.count_projects
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def self.count_projects
+    Project.count
+  end
+  
 end
