d849ae2f01aee85ad789fd806e6725c978ec4de1
Martin Chavez <martinchavez1203@gmail.com>
2024-02-13 16:33:25 -0700

    US 1 complete

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Project show page" do
+  
+  describe 'US 1' do
+    describe 'project show page' do
+      it 'shows project names and material' do
+        challenge_1 = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        challenge_2 = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        project_1 = challenge_1.projects.create(name: "News Chic", material: "Newspaper")
+        project_2 = challenge_1.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+        visit "/projects/#{project_1.id}"
+
+        expect(page).to have_content(project_1.name)
+        expect(page).to have_content(project_1.material)
+        expect(page).to have_content(challenge_1.theme)
+
+        
+
+
+      end
+    end
+  end
+end
