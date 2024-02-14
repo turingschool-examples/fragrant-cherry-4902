c0edaa52810a2db67a76a07a73cf28140e6ae36d
Logan Finnegan <logantfinnegan@protonmail.com>
2024-02-13 18:32:39 -0600

    mandatory model testing

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -31,7 +31,33 @@ RSpec.describe Contestant, type: :model do
       ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
       ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
 
-      expect(Contestant.count).to eq(4)
+      expect(Contestant.contestant_count).to eq(4)
+    end
+
+    it ".average_exp" do 
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 1)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 1)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 1)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 1)
+
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+      expect(Contestant.average_exp).to eq(1)
     end
   end 
 end
