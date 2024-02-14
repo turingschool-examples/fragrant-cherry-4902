0b025c5d78ec4a151394f4b5fe9fe3da3fb5aa1d
Martin Chavez <martinchavez1203@gmail.com>
2024-02-13 18:13:28 -0700

    US 3 complete

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe "Project show page" do
       end
     end
   end
+
+  describe 'US 3' do
+    describe 'Projects show' do
+      it 'counts of the number of contestants on this project' do
+        challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+        project = challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+        contestant_1 = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        contestant_2 = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+        ContestantProject.create(contestant_id: contestant_1.id, project_id: project.id)
+        ContestantProject.create(contestant_id: contestant_2.id, project_id: project.id)
+
+        visit "/projects/#{project.id}"
+
+        expect(page).to have_content("Number of Contestants: 2")
+      end
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe '#instance method' do
+    it 'counts number of contestants on a project' do
+      challenge_1 = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      project_1 = challenge_1.projects.create(name: "News Chic", material: "Newspaper")
+      project_2 = challenge_1.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      contestant_1 = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+
+      ContestantProject.create(contestant_id: contestant_1.id, project_id: project_1.id)
+      ContestantProject.create(contestant_id: contestant_1.id, project_id: project_2.id)
+      
+      expect(project_1.contestant_count).to eq(1)
+    end
+  end
+
 end
