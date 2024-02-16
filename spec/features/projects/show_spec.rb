2dd6317bf96a48c519ac94f9e29af8934651389e
Barry Allahyar <barrya@gmail.com>
2024-02-15 20:27:43 -0600

    Catch and fix spelling errors and put in all the forgotten @ signs >.<

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,19 +1,17 @@
 require "rails_helper"
 
-RSpec.describe "Project Show Page" do
+RSpec.describe "Project Show Page", type: :feature do
   before :each do
-    @project_1 = challenge_1.projects.create!(name: "News Chic", material: "Newspaper")
-    @project_2 = challenge_1.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
-    
     @challenge_1 = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
-    @challenge_2 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @project_1 = @challenge_1.projects.create!(name: "News Chic", material: "Newspaper")
   end
 
   it "displays project's name, material, and theme of the challenge when visiting project show page" do
-    visit "/projects/#{project_1.id}"
+    visit "/projects/#{@project_1.id}"
 
-    expect(page).to have_content(@project_1.name)
-    expect(page).to have_content(@project_1.material)
-    expect(page).to have_content(@challenge_1.theme)
+    expect(page).to have_content("Project Name: #{@project_1.name}")
+    expect(page).to have_content("Material: #{@project_1.material}")
+    expect(page).to have_content("Challenge Theme: #{@challenge_1.theme}")
   end
 end
